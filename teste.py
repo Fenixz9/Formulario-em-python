2582ca574c2ef3fa78d37741cb782f3964de8e87
Fenixz9 <145057390+Fenixz9@users.noreply.github.com>
2024-06-27 07:54:15 -0300

    Mudança2 teste.py
    mudei algumas variaveis, troquei nomes de variaveis e coloquei um função principal que guarda todas as outras. Vinicius

--- teste.py
+++ teste.py
@@ -1,22 +1,80 @@
-import math
+def cadastro_devs():
+    print("Cadastro de desenvolvedores")
+    nome = input("Digite seu nome: ")
+    apelido = input("Digite seu apelido: ")
+    ids = int(input("Digite seu ID (apenas números): "))
+    ativo = False  # Alterado para booleano para simplicidade
 
-print("Cadastro de devs")
+    def ja_cadastrado():
+        nonlocal nome, apelido, ids, ativo
 
-nome = input("Coloque seu nome, dev: ")
-apelido = input("coloque seu apelido, dev: ")
-ids = int(input("Coloque o seu id (apenas numeros), dev: "))
-ativo = "nao"
+        if nome == "Vinicius dos Santos Pereira":
+            pass
 
-def ja_cadastrado():
-    if nome == "Vinicius dos Santos Pereira":
-        pass
-    if apelido == "Tanmo" or apelido == "Vnzin" or apelido == "Hood":
-        print("Apelido ja cadastrado, por favor coloque outro.")
-        apelido = input("coloque seu apelido, dev: ")
-        ativo = "simAp"
-    if ids == 3225 or 4041:
-        ativo = "simId"
-        print("ID ja cadastrado!!")
-        ids = int(input("Coloque o seu id (apenas numeros), dev: "))
+        if apelido in ["Tanmo", "Vnzin", "Hood"]:
+            print("Apelido já cadastrado. Por favor, escolha outro.")
+            apelido = input("Digite seu apelido novamente: ")
+            ativo = True
 
-ja_cadastrado()
+        if nome == apelido:
+            print("O nome e o apelido são iguais.")
+            nome = input("Digite seu nome novamente: ")
+            apelido = input("Digite seu apelido novamente: ")
+
+        if ids in [3225, 4041]:
+            ativo = True
+            print("ID já cadastrado!!")
+            ids = int(input("Digite seu ID novamente: "))
+
+    ja_cadastrado()
+    print("__________________________________________________")
+    print(nome)
+    print(apelido)
+    print(ids)
+
+    print("__________________________________________________")
+    print("Agora vamos confirmar que você é humano jogando este jogo.")
+
+    def jogo():
+        print("Nas alternativas corretas, coloque V; nas erradas, coloque F.")
+        print("Espero que você seja humano...")
+        print("__________________________________________________")
+        pergunta1 = input("35 + 30 = 65? (V/F): ").upper()
+        pergunta2 = input("A letra 'A' é uma vogal? (V/F): ").upper()
+        ativacao = 0
+        robo = 0
+        verify = "nao"
+
+        if pergunta1 == "V":
+            print("Quase lá...")
+            print("__________________________________________________")
+            ativacao += 1
+        else:
+            print("Será que você é um robô...")
+            print("__________________________________________________")
+            robo += 1
+
+        if pergunta2 == "V":
+            print("Quase lá...")
+            print("__________________________________________________")
+            ativacao += 1
+        else:
+            print("Você errou, será que você é um robô...")
+            print("__________________________________________________")
+            robo += 1
+
+        if ativacao == 2:
+            print("Pronto, verificamos que você é um humano. Sua conta vai ser criada...")
+            print("__________________________________________________")
+            verify = "verificado"
+
+        if robo == 2:
+            print("-_- Você é um robô... se não verifique mais uma vez.")
+
+        if verify == "verificado":
+            print("__________________________________________________")
+            print("Sua conta foi criada. Aproveite!")
+
+    jogo()
+
+cadastro_devs()
